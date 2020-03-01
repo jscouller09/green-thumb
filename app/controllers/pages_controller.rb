6403e0d10361732113f3ec36ebdc26c186291497
James Scouller <jscouller09@gmail.com>
2020-03-01 13:37:41 -0500

    Initial setup of cloudinary, pundit and redirect to github landing page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
+    redirect_to "https://jscouller09.github.io/green-thumb-landing-page/"
   end
 end
