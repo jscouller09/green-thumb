5970ebd7caaa7906ebce98db3569ca818c1567b8
James Scouller <jscouller09@gmail.com>
2020-03-03 07:51:25 -0500

    Fixed typo

--- app/models/weather_station.rb
+++ app/models/weather_station.rb
@@ -1,7 +1,7 @@
 require 'open-uri'
 
 class WeatherStation < ApplicationRecord
-validates :name, presence: true
+  validates :name, presence: true
   validates :country, presence: true, format: { with: /[A-Z]{2}/ }
   validates :lat,
             numericality: { greater_than_or_equal_to: -90,
