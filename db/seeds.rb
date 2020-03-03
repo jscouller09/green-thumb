b408ac6cf0f658f870af1e54cd0542117944dfd9
James Scouller <jscouller09@gmail.com>
2020-03-03 18:27:44 -0500

    Added generic code to generate model seeds from csv files

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,20 +6,35 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+require 'csv'
 
-# set admin user
-User.destroy_all
-User.create!(email: 'admin@greenthumb.rocks', password: '123456', admin: true)
-puts "Created admin user!"
-
-# initialize weather station (if not already in DB)
-chc_stn = { id: 7_910_036, lon: 172.745865, lat: -43.645779,
-            name: 'Christchurch City', country: 'NZ' }
-begin
-  WeatherStation.find(chc_stn[:id]).nil?
-rescue ActiveRecord::RecordNotFound
-  WeatherStation.create!(chc_stn)
-  puts "Added Christchurch 7910036 station to DB!"
-else
-  puts "Skipping Christchurch 7910036 station as already in DB..."
+def generate_model_from_csv(model_class, csv_file)
+  csv_options = { col_sep: ',',
+                  quote_char: '"',
+                  headers: :first_row,
+                  converters: :numeric,
+                  header_converters: :symbol }
+  CSV.read("db/#{csv_file}", csv_options).each_with_index do |row, i|
+    # grab all columns and generate instance of the model
+    args = row.to_h
+    model = model_class.new(args)
+    begin
+      valid = model.save
+    rescue ActiveRecord::RecordNotUnique
+      puts "Couldn't create row #{i} from #{csv_file}. Already in DB!"
+    else
+      unless valid
+        puts "Couldn't create row #{i} from #{csv_file}. Validation error?"
+        model.errors.messages.each { |k, v| puts "\t#{k}: #{v}"}
+      else
+        puts "Made model for row #{i} from #{csv_file}."
+      end
+    end
+  end
 end
+
+# generate weather stations
+generate_model_from_csv(WeatherStation, 'weather_stations.csv')
+
+# generate users
+generate_model_from_csv(WeatherStation, 'users.csv')
