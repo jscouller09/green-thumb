be3d233d984284b7060b49a84de8bda118cc32cd
Rose Grondin <rose.grondin@gmail.com>
2020-03-03 14:51:30 -0500

    add plant type validation

--- app/models/plant_type.rb
+++ app/models/plant_type.rb
@@ -1,2 +1,23 @@
 class PlantType < ApplicationRecord
+  #associations
+  belongs_to :plant
+
+  #a plant type as a name
+  validates :name, presence: true
+
+  # a plant type spacing can only be an integer > 0
+  validates :spacing_mm, numericality: { only_integer: true, greater_than: 0 }
+  validates :height_mm, numericality: { only_integer: true, greater_than: 0 }
+
+  # a plant type kc is a float greater or = to 0
+  validates :kc_ini, numericality: { greater_than_or_equal_to: 0 }
+  validates :kc_mid, numericality: { greater_than_or_equal_to: 0 }
+  validates :kc_end, numericality: { greater_than_or_equal_to: 0 }
+
+  # a plant type L days can only be integer and > 0
+  validates :L_ini_days, numericality: { only_integer: true, greater_than: 0 }
+  validates :L_dev_days, numericality: { only_integer: true, greater_than: 0 }
+  validates :L_mid_days, numericality: { only_integer: true, greater_than: 0 }
+  validates :L_end_days, numericality: { only_integer: true, greater_than: 0 }
+
 end
