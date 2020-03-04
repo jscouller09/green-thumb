5cf45eb953af3f4df4abd10738756cd6065380b0
James Scouller <jscouller09@gmail.com>
2020-03-04 13:50:40 -0500

    Fixed error allowing nil for kc_ini vals

--- app/models/plant_type.rb
+++ app/models/plant_type.rb
@@ -10,9 +10,9 @@ class PlantType < ApplicationRecord
   validates :height_mm, numericality: { only_integer: true, greater_than: 0 }
 
   # a plant type kc is a float greater or = to 0 (note default is set at 1.0)
-  validates :kc_ini, numericality: { greater_than_or_equal_to: 0, allow_nil: true }
-  validates :kc_mid, numericality: { greater_than_or_equal_to: 0, allow_nil: true }
-  validates :kc_end, numericality: { greater_than_or_equal_to: 0, allow_nil: true }
+  validates :kc_ini, numericality: { greater_than_or_equal_to: 0 }
+  validates :kc_mid, numericality: { greater_than_or_equal_to: 0 }
+  validates :kc_end, numericality: { greater_than_or_equal_to: 0 }
 
   # a plant type L days can only be integer and > 0
   validates :l_ini_days, numericality: { only_integer: true, greater_than: 0 }
