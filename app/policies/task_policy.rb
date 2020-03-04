9112ebdeabc050b599c951dd7294ddea2423f0c1
Antoine Bonifacj <abonifacj63@gmail.com>
2020-03-04 15:11:15 -0500

    adding comments

--- app/policies/task_policy.rb
+++ app/policies/task_policy.rb
@@ -2,29 +2,29 @@ class TaskPolicy < ApplicationPolicy
   class Scope < Scope
 
     def resolve
-    # an admin can see all task of every user
+    # An admin can see all task of every user
       if user.admin?
         scope.all
-    # normal user can only see their own tasks
+    # Normal user can only see their own tasks
       else
         scope.where(user: user)
       end
 
     end
   end
-
+    # only the current user can create a task.
   def create?
     user.present?
   end
-
+    # Only and admin or the current user can update a task
   def update?
     user.admin? || record.user == user
   end
-
+    # Same policies as update
   def destroy?
     update?
   end
-
+    # Same policies as update
   def mark_as_complete?
     update?
   end
