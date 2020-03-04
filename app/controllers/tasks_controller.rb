7bd863ed2ac18d6d41081cf6654f7b6e701e9672
Antoine Bonifacj <abonifacj63@gmail.com>
2020-03-04 13:07:24 -0500

    adding private safe params

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,25 +7,40 @@ class TasksController < ApplicationController
 
   # POST  /tasks/
   def create
-    @task = Task.new
+    @task = Task.new(task_params)
     authorize @task
+    @task.user = current_user
+    @task.save
+    redirect_to tasks_path
   end
 
   # PATCH /tasks/:id
   def update
     @task = Task.find(params[:id])
     authorize @task
+    @task.save
   end
 
 
   # DELETE  /tasks/:id
   def destroy
+    @task = Task.find(params[:id])
+    authorize @task
+    @task.destroy
+    redirect_to dashboard_path
 
   end
 
   # PATCH tasks/:id/complete
   def mark_as_complete
   end
+
+private
+
+def task_params
+    params.require(:task).permit(:description)
+  end
+
 end
 
 
