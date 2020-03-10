9026357cc7ed5bb14d5a43b7d33ba71402f1bdb0
Christina BarrettiSigal <CBarrettiSigal@gmail.com>
2020-03-10 15:20:38 -0400

    implemented confirmation modal for across app to replace default popups

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,6 +2,7 @@ import "bootstrap";
 import { initAutocomplete } from "../plugins/init_autocomplete";
 import { init_plant_dragging } from "../plugins/init_plant_dragging";
 import "../plugins/flatpickr"
+import "../plugins/modal_confirm"
 // set autocomplete on garden address field if it exists
 initAutocomplete('garden_address');
 
