a61575b33335ecfc4d1eceaf23bb4ab90984237e
James Scouller <jscouller09@gmail.com>
2020-03-12 15:46:13 -0400

    Fixed error with no direction if no deg given from weather station

--- app/models/weather_station.rb
+++ app/models/weather_station.rb
@@ -388,22 +388,25 @@ class WeatherStation < ApplicationRecord
 
   def wind_direction(deg)
     # convert wind direction in degrees to cardinal direction
-    return 'N' if deg <= 11.25 || deg > 348.75
-    return 'NNE' if deg > 11.25 && deg <= 33.75
-    return 'NE' if deg > 33.75 && deg <= 56.25
-    return 'ENE' if deg > 56.25 && deg <= 78.75
-    return 'E' if deg > 78.75 && deg <= 101.25
-    return 'ESE' if deg > 101.25 && deg <= 123.75
-    return 'SE' if deg > 123.75 && deg <= 146.25
-    return 'SSE' if deg > 146.25 && deg <= 168.75
-    return 'S' if deg > 168.75 && deg <= 191.25
-    return 'SSW' if deg > 191.25 && deg <= 213.75
-    return 'SW' if deg > 213.75 && deg <= 236.25
-    return 'WSW' if deg > 236.25 && deg <= 258.75
-    return 'W' if deg > 258.75 && deg <= 281.25
-    return 'WNW' if deg > 281.25 && deg <= 303.75
-    return 'NW' if deg > 303.75 && deg <= 326.25
-    return 'NNW' if deg > 326.25 && deg <= 348.75
+    unless deg.nil?
+      return 'N' if deg <= 11.25 || deg > 348.75
+      return 'NNE' if deg > 11.25 && deg <= 33.75
+      return 'NE' if deg > 33.75 && deg <= 56.25
+      return 'ENE' if deg > 56.25 && deg <= 78.75
+      return 'E' if deg > 78.75 && deg <= 101.25
+      return 'ESE' if deg > 101.25 && deg <= 123.75
+      return 'SE' if deg > 123.75 && deg <= 146.25
+      return 'SSE' if deg > 146.25 && deg <= 168.75
+      return 'S' if deg > 168.75 && deg <= 191.25
+      return 'SSW' if deg > 191.25 && deg <= 213.75
+      return 'SW' if deg > 213.75 && deg <= 236.25
+      return 'WSW' if deg > 236.25 && deg <= 258.75
+      return 'W' if deg > 258.75 && deg <= 281.25
+      return 'WNW' if deg > 281.25 && deg <= 303.75
+      return 'NW' if deg > 303.75 && deg <= 326.25
+      return 'NNW' if deg > 326.25 && deg <= 348.75
+    end
+    return ''
   end
 
   def send_query(url)
